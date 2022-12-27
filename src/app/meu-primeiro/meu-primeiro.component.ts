0d60ca4ff84b2b0e7865d68d8fb86692837fefcf
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-27 20:26:48 -0300

    Primeiro commit

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu Primeiro Componente com Angular</p>
+  `
+})
+export class MeuPrimeiroComponent {
+
+}

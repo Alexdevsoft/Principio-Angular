7e70060d5390338b1155061b384cf86eaf5ede80
Alexdevsoft <alexhavilla2022@gmail.com>
2023-01-02 15:51:26 -0300

    Service

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Angular'];
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://google.com'
+    /*for(let i=0; i<this.cursos.length; i++){
+      let cursos = this.cursos[i];
+    }
+    */
+    //var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
+
+   }
 
   ngOnInit(): void {
   }
